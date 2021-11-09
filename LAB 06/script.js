41d4c7d7c534068c162f0c131858f433bb7744da
Henrique Dias <hvdias2015@gmail.com>
2021-11-09 12:04:33 -0300

    atualizando o git

--- LAB 06/script.js
+++ LAB 06/script.js
@@ -1,226 +1,155 @@
-const canvas = document.getElementById('game');
-const ctx = canvas.getContext('2d');
+class Conta {
+    constructor(id, nome, saldoInicial){
+        this.id = id;
+        this.nome = nome;
+        this.saldo = Number(saldoInicial);
+    }
 
-let score;
-let scoreText;
-let highscore;
-let highscoreText;
-let player;
-let gravity;
-let obstacles = [];
-let gameSpeed;
-let keys = {};
+    sacar(valor){
+        this.saldo -= (Number(valor));
+    }
 
-// Event Listeners
-document.addEventListener('keydown', function (evt) {
-  keys[evt.code] = true;
+    depositar(valor){
+        this.saldo += (Number(valor));
+    }
+}
+
+class Banco {
+    constructor(){
+        this.contas = [];
+    }
+
+    transferir(contaEnvio,idDestino, valor){
+        this.contas.forEach(conta => {
+            if(conta.id === idDestino){
+                conta.depositar(valor);
+                contaEnvio.sacar(valor);
+                return 1;
+            }
+        });
+        return 0;
+    }
+
+    verificaId(id){
+        let ret = 1;
+        this.contas.forEach(conta => {
+            if(conta.id == id) ret = 0;
+        });
+        return ret;
+    }
+}
+
+var banco = new Banco();
+banco.contas.push(new Conta(0, "Henrique Dias", 0));
+var contaSelecionada = null;
+
+const deposito = document.getElementById("deposito");
+const btnDeposito = document.getElementById("btnDeposito");
+
+const saque = document.getElementById("saque");
+const btnSaque = document.getElementById("btnSaque");
+
+const idCadastro = document.getElementById("idCadastro");
+const nomeCadastro = document.getElementById("nomeCadastro");
+const depositoCadastro = document.getElementById("depositoCadastro");
+const btnCadastro = document.getElementById("btnCadastro");
+
+const idTroca = document.getElementById("idTroca");
+const btnTrocarConta = document.getElementById("btnTrocarConta");
+
+const valorTransfere = document.getElementById("valorTransfere");
+const idTransfere = document.getElementById("idTransfere");
+const btnTransfere = document.getElementById("btnTransfere");
+
+
+const formatter = new Intl.NumberFormat('pt-BR', {
+    style: 'currency',
+    currency: 'BRL',
+    minimumFractionDigits: 2,
+  });
+
+btnTransfere.addEventListener('click', () =>{
+    banco.transferir(contaSelecionada, idTransfere.value, valorTransfere.value);
+    let totalBalanceFormatted = formatter.format(contaSelecionada.saldo);
+    document.getElementById("saldoAtual").innerHTML = "Saldo " +totalBalanceFormatted;
+    document.getElementById("id").innerHTML = "ID " + contaSelecionada.id;
+    document.getElementById("usuario").innerHTML = "Usuario: "+ contaSelecionada.nome;
 });
-document.addEventListener('keyup', function (evt) {
-  keys[evt.code] = false;
+
+btnTrocarConta.addEventListener('click', () =>{
+    banco.contas.forEach(conta =>{
+        if(conta.id == idTroca.value) contaSelecionada = conta;
+    })
+    let totalBalanceFormatted = formatter.format(contaSelecionada.saldo);
+    document.getElementById("saldoAtual").innerHTML = "Saldo " +totalBalanceFormatted;
+    document.getElementById("id").innerHTML = "ID " + contaSelecionada.id;
+    document.getElementById("usuario").innerHTML = "Usuario: "+ contaSelecionada.nome;
 });
 
-class Player {
-  constructor (x, y, w, h, c) {
-    this.x = x;
-    this.y = y;
-    this.w = w;
-    this.h = h;
-    this.c = c;
+btnCadastro.addEventListener('click', () =>{
+    if(isNaN(idCadastro.value)){
+        alert("ID invalido");
+        return 0;
+    }else if(isNaN(depositoCadastro.value) || Number(depositoCadastro.value)<0){
+        alert("Deposito Inicial Invalido");
+        return 0;
+    }else if(banco.verificaId(idCadastro.value) === 0){
+        alert("ID ja cadastrado");
+        return 0;
+    }else{
+        contaSelecionada = new Conta(idCadastro.value, nomeCadastro.value, depositoCadastro.value);
+        banco.contas.push(contaSelecionada);
+        let totalBalanceFormatted = formatter.format(contaSelecionada.saldo);
+        document.getElementById("saldoAtual").innerHTML = "Saldo " +totalBalanceFormatted;
+        document.getElementById("id").innerHTML = "ID " + contaSelecionada.id;
+        document.getElementById("usuario").innerHTML = "Usuario: "+ contaSelecionada.nome;
+        return 1;
+    }
+});
 
-    this.dy = 0;
-    this.jumpForce = 15;
-    this.originalHeight = h;
-    this.grounded = false;
-    this.jumpTimer = 0;
-  }
-
-  Animate () {
-    // Jump
-    if (keys['Space'] || keys['KeyW']) {
-      this.Jump();
+btnDeposito.addEventListener('click', () => {
+    if(contaSelecionada === null){
+        alert("Selecione uma conta");
+    }
+    if (isNaN(deposito.value)) {
+        alert("Coloque um numero valido.");
+        return deposito.value = '';
     } else {
-      this.jumpTimer = 0;
-    }
-
-    if (keys['ShiftLeft'] || keys['KeyS']) {
-      this.h = this.originalHeight / 2;
-    } else {
-      this.h = this.originalHeight;
-    }
-
-    this.y += this.dy;
-
-    // Gravity
-    if (this.y + this.h < canvas.height) {
-      this.dy += gravity;
-      this.grounded = false;
-    } else {
-      this.dy = 0;
-      this.grounded = true;
-      this.y = canvas.height - this.h;
-    }
-
-    this.Draw();
-  }
-
-  Jump () {
-    if (this.grounded && this.jumpTimer == 0) {
-      this.jumpTimer = 1;
-      this.dy = -this.jumpForce;
-    } else if (this.jumpTimer > 0 && this.jumpTimer < 15) {
-      this.jumpTimer++;
-      this.dy = -this.jumpForce - (this.jumpTimer / 50);
-    }
-  }
-
-  Draw () {
-    ctx.beginPath();
-    ctx.fillStyle = this.c;
-    ctx.fillRect(this.x, this.y, this.w, this.h);
-    ctx.closePath();
-  }
-}
-
-class Obstacle {
-  constructor (x, y, w, h, c) {
-    this.x = x;
-    this.y = y;
-    this.w = w;
-    this.h = h;
-    this.c = c;
-
-    this.dx = -gameSpeed;
-  }
-
-  Update () {
-    this.x += this.dx;
-    this.Draw();
-    this.dx = -gameSpeed;
-  }
-
-  Draw () {
-    ctx.beginPath();
-    ctx.fillStyle = this.c;
-    ctx.fillRect(this.x, this.y, this.w, this.h);
-    ctx.closePath();
-  }
-}
-
-class Text {
-  constructor (t, x, y, a, c, s) {
-    this.t = t;
-    this.x = x;
-    this.y = y;
-    this.a = a;
-    this.c = c;
-    this.s = s;
-  }
-
-  Draw () {
-    ctx.beginPath();
-    ctx.fillStyle = this.c;
-    ctx.font = this.s + "px sans-serif";
-    ctx.textAlign = this.a;
-    ctx.fillText(this.t, this.x, this.y);
-    ctx.closePath();
-  }
-}
-
-// Game Functions
-function SpawnObstacle () {
-  let size = RandomIntInRange(20, 70);
-  let type = RandomIntInRange(0, 1);
-  let obstacle = new Obstacle(canvas.width + size, canvas.height - size, size, size, '#2484E4');
-
-  if (type == 1) {
-    obstacle.y -= player.originalHeight - 10;
-  }
-  obstacles.push(obstacle);
-}
-
-
-function RandomIntInRange (min, max) {
-  return Math.round(Math.random() * (max - min) + min);
-}
-
-function Start () {
-  canvas.width = window.innerWidth;
-  canvas.height = window.innerHeight;
-
-  ctx.font = "20px sans-serif";
-
-  gameSpeed = 3;
-  gravity = 1;
-
-  score = 0;
-  highscore = 0;
-  if (localStorage.getItem('highscore')) {
-    highscore = localStorage.getItem('highscore');
-  }
-
-  player = new Player(25, 0, 50, 50, '#FF5858');
-
-  scoreText = new Text("Score: " + score, 25, 25, "left", "#212121", "20");
-  highscoreText = new Text("Highscore: " + highscore, canvas.width - 25, 25, "right", "#212121", "20");
-
-  requestAnimationFrame(Update);
-}
-
-let initialSpawnTimer = 200;
-let spawnTimer = initialSpawnTimer;
-function Update () {
-  requestAnimationFrame(Update);
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-  spawnTimer--;
-  if (spawnTimer <= 0) {
-    SpawnObstacle();
-    console.log(obstacles);
-    spawnTimer = initialSpawnTimer - gameSpeed * 8;
+        if (deposito.value < 0.01 || deposito.value > 10000) {
+            alert("Deposite entre R$0.01 e R$10,000.")
+            return deposito.value = '';
+        } else {
     
-    if (spawnTimer < 60) {
-      spawnTimer = 60;
+            contaSelecionada.depositar(deposito.value);
+
+        let totalBalanceFormatted = formatter.format(contaSelecionada.saldo);
+        document.getElementById("saldoAtual").innerHTML ="Saldo " + totalBalanceFormatted;
     }
-  }
-
-  // Spawn Enemies
-  for (let i = 0; i < obstacles.length; i++) {
-    let o = obstacles[i];
-
-    if (o.x + o.w < 0) {
-      obstacles.splice(i, 1);
-    }
-
-    if (
-      player.x < o.x + o.w &&
-      player.x + player.w > o.x &&
-      player.y < o.y + o.h &&
-      player.y + player.h > o.y
-    ) {
-      obstacles = [];
-      score = 0;
-      spawnTimer = initialSpawnTimer;
-      gameSpeed = 3;
-      window.localStorage.setItem('highscore', highscore);
-    }
-
-    o.Update();
-  }
-
-  player.Animate();
-
-  score++;
-  scoreText.t = "Score: " + score;
-  scoreText.Draw();
-
-  if (score > highscore) {
-    highscore = score;
-    highscoreText.t = "Highscore: " + highscore;
-  }
-  
-  highscoreText.Draw();
-
-  gameSpeed += 0.003;
 }
+    
+});
 
-Start();
+// accept withdrawals from user, store withdrawals in array
+btnSaque.addEventListener('click', () => {
+    if(contaSelecionada === null){
+        alert("Selecione uma conta");
+    }
+    // checks if withdrawal is a number
+    if (isNaN(saque.value)) {
+        alert("Coloque um numero valido.");
+        return saque.value = '';
+    } else {
+
+        // checks if withdrawal meets parameters
+        if (saque.value > contaSelecionada.saldo) {
+            alert("Sua conta não pode ficar sem saldo");
+            return saque.value = '';
+        } else {
+
+            contaSelecionada.sacar(saque.value);
+
+            let totalBalanceFormatted = formatter.format(contaSelecionada.saldo);
+            document.getElementById("saldoAtual").innerHTML = "Saldo " + totalBalanceFormatted;
+    }
+}
+});
